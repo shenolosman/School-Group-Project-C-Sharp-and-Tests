80293964c9029fe7ec4f9c175a67da9c2f27bd3d
Shenol Osman <7887878+shenolosman@users.noreply.github.com>
2021-10-05 14:06:37 +0200

    Implamentera Interface

---
+++ Manager/RealTime.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manager
+{
+    public class RealTime: ITime
+
+    {
+    public DateTime Now
+    {
+        get
+        {
+            return DateTime.Now;
+        }
+    }
+    }
+}
---
+++ Test/MockTime.cs
@@ -0,0 +1,29 @@
+﻿using Manager;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    public class MockTime : ITime
+    {
+        private DateTime _fakeNow;
+        public MockTime()
+        {
+            _fakeNow = DateTime.Now;
+        }
+        public DateTime Now
+        {
+            get
+            {
+                return _fakeNow;
+            }
+        }
+        public void SetNowTo(DateTime newNow)
+        {
+            _fakeNow = newNow;
+        }
+    }
+}
