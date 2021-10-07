bd7eb7c4741823d32f6112f082f24d7510ff7b07
Shenol Osman <7887878+shenolosman@users.noreply.github.com>
2021-10-07 15:13:28 +0100

    [Red] testing investering account with annual cashout

--- Manager/Investeringskonto.cs
+++ Manager/Investeringskonto.cs
@@ -7,10 +7,12 @@ using System.Threading.Tasks;
 namespace Manager
 {
    public class Investeringskonto : BankKonto
-    {
+   {
+       public ITime itime;
         //Investeringskonto som tillåter ett uttag om året
         public Investeringskonto(int balance) : base(balance)
         {
+            //itime = tid;
         }
 
         public override bool CanTakeOutMoney(int amount)
--- Test/InvesteringkontoTest.cs
+++ Test/InvesteringkontoTest.cs
@@ -3,15 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Manager;
 using Xunit;
 
 namespace Test
 {
     public class InvesteringkontoTest
     {
-        [Fact]
-        public void FunkarDet()
+        private MockTime ttime;
+
+        public InvesteringkontoTest()
         {
+            ttime = new MockTime();
+        }
+        [Fact]
+        public void Test_InvesteringTid()
+        {
+            ttime.time = 1;
+            var investera = new Investeringskonto(1000);
+
+            var withdraw = investera.CanTakeOutMoney(500);
+            Assert.True(withdraw);
+
+            withdraw = investera.CanTakeOutMoney(200);
+            Assert.True(withdraw);
+            ttime.time = Time.YearInMilisec + 100;
+
+            withdraw = investera.CanTakeOutMoney(200);
+            Assert.False(withdraw);
 
         }
 
