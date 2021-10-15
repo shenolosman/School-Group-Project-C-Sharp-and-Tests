54f09ec57589ff29542dcfc598a2cee74f0087e8
Andreas Larsson <andreas.larsson3@learnet.se>
2021-10-15 15:28:15 +0200

    [Refactor]

--- Manager/BankKonto.cs
+++ Manager/BankKonto.cs
@@ -15,31 +15,43 @@ namespace Manager
         protected int depositLimit;
         protected int withdrawLimit;
         protected long withdrawTime;
-        protected BankKonto(int balance)
-        {
-            Balance = balance;
-        }
-        protected BankKonto(ITime time)
+
+        protected BankKonto(ITime time, int balance)
         {
             tid = time;
+            depositLimit = 15000;
+            Balance = balance;
         }
-        
 
+        public BankKonto(ITime time) {
+            tid = time;
+            Balance = 0;
+            depositLimit = 15000;
+        }
+        public bool Deposit(int amount)
+        {
+            if (amount <= 0)
+                return false;
+
+            if (tid.GetTime() > depositTime + Time.DayInMillisec)
+            {
+                depositLimit = 15000;
+                depositTime = tid.GetTime();
+            }
+            if (amount > depositLimit)
+                return false;
+
+            depositLimit -= amount;
+            Balance += amount;
+            return true;
+        }
         public abstract bool Withdraw(int amount);
     }
     public class Investeringskonto : BankKonto
     {
-        public ITime itime;
-        //Investeringskonto som tillåter ett uttag om året
-        public Investeringskonto(int balance) : base(balance)
-        {
-            //itime = tid;
-        }
-
         public Investeringskonto(ITime time) : base(time)
         {
             withdrawLimit = 1;
-            depositLimit = 15000;
         }
 
         public override bool Withdraw(int amount)
@@ -61,33 +73,13 @@ namespace Manager
             withdrawLimit--;
             Balance -= amount;
             return true;
-
         }
-        public bool Deposit(int amount)
-        {
-            if (amount <= 0)
-                return false;
-
-            if (tid.GetTime() > depositTime + Time.YearInMilisec)
-            {
-                depositLimit = 15000;
-                depositTime = tid.GetTime();
-            }
-
-            if (amount > depositLimit)
-                return false;
-
-            depositLimit -= amount;
-            Balance += amount;
-            return true;
-        }
+        
     }
     public class KreditKonto : BankKonto
     {
         private int _credit;
-        //Kreditkonto som tillåter kredit över en viss gräns
-        public KreditKonto(int balance) : base(balance) { }
-        public KreditKonto(int balance, int credit) : this(balance) {
+        public KreditKonto(int balance, int credit) : base(null, balance) {
             this._credit = credit;
         }
         public override bool Withdraw(int amount)
@@ -108,18 +100,15 @@ namespace Manager
                     Balance -= amount;
                 }
             }
-
             return takeOutMoney;
         }
     }
     public class Lönekonto : BankKonto
     {
-        //Lönekonto som tillåter obegränsat antal uttag
-
-        public Lönekonto(int balance) : base(balance)
+        public Lönekonto(ITime time) : base(time)
         {
-            depositLimit = 15000; }
-        public Lönekonto(ITime time) : base(time) { depositLimit = 15000; }
+            depositLimit = 15000;
+        }
         public override bool Withdraw(int amount)
         {
             bool takeOutMoney = amount <= Balance;
@@ -128,61 +117,15 @@ namespace Manager
             {
                 Balance -= amount;
             }
-
             return takeOutMoney;
         }
-
-        public bool Deposit(int amount)
-        {
-            if (amount <= 0)
-                return false;
-
-            if (tid.GetTime() > depositTime + Time.DayInMillisec)
-            {
-                depositLimit = 15000;
-                depositTime = tid.GetTime();
-            }
-            
-            if (amount > depositLimit)
-                return false;
-
-            depositLimit -= amount;
-            Balance += amount;
-            return true;
-        }
-
     }
     public class Sparkonto : BankKonto
     {
-        //Sparkonto som tillåter max 5 uttag om året (fler kan göras men då kostar det 1% av uttaget)
-        private int gånger = 0;
-        readonly DateTime resetTime = DateTime.Now - TimeSpan.FromDays(-365);
         public Sparkonto(ITime time) : base(time)
         {
             withdrawLimit = 5;
-            depositLimit = 15000;
         }
-
-        public bool Deposit(int amount)
-        {
-            if (amount <= 0)
-                return false;
-
-            if (tid.GetTime() > depositTime + Time.DayInMillisec)
-            {
-                depositLimit = 15000;
-                depositTime = tid.GetTime();
-            }
-
-            if (amount > depositLimit)
-                return false;
-
-            depositLimit -= amount;
-            Balance += amount;
-            return true;
-        }
-
-
         public override bool Withdraw(int amount)
         {
             if (amount <= 0 || amount > Balance)
@@ -198,11 +141,9 @@ namespace Manager
                 // casting
                 Balance -= (int) Math.Ceiling(((double) amount * 0.01));
             }
-
             withdrawLimit--;
             Balance -= amount;
             return true;
-
         }
     }
 }
